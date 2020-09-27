5a25787115caf9b7f1c0101e00b5e4eedd663f0e
slr <nilaygupta1998@gmail.com>
2020-09-28 00:56:26 +0530

    password encryption added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const mysqlConnection=require("../connection");
+const bcrypt=require('bcrypt');
 
 router.get('/getAll', (req,res) =>{
     const sql="select * from UserInfo";
@@ -10,17 +11,34 @@ router.get('/getAll', (req,res) =>{
     });
 });
 
-router.post('/register', (req,res) =>{
+router.post('/login',(req,res)=>{
     const userData={
-        name:req.body.name,
         email:req.body.email,
-        password:req.body.password,
-        phone:req.body.phone
+        password:req.body.password
     };
-    mysqlConnection.query('select email from UserInfo where email=?',req.body.email,(err,rows,fields)=>{
-        if(err) throw err;
-        if(rows.length > 0) return res.json({status:'0',message:'Student already registered.'});
 
+    mysqlConnection.query('select * from UserInfo where email=?',req.body.email,async (err,rows,fields)=>{
+        if(err) throw err;
+        if(rows.length == 0) return res.json({status:'0',message:'Invalid Email or Password'});
+        const validPassword = await bcrypt.compare(req.body.password,rows[0].password);
+        if (!validPassword) return res.json({status:0,message:'Invalid email or password.'})
+        return res.json({status:'1', message:'Login Success', name:rows[0].name, email:rows[0].email });
+    });
+});
+
+router.post('/register', (req,res) =>{
+    mysqlConnection.query('select email from UserInfo where email=?',req.body.email, async (err,rows,fields)=>{
+        if(err) throw err;
+        if(rows.length > 0) return res.json({status:'0',message:'Email already registered.'});
+
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+        const userData={
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            phone:req.body.phone
+        };
         const sql="INSERT INTO UserInfo set ?";
         mysqlConnection.query(sql,userData, (err,rows,fields)=>{
             if(err) throw err;
