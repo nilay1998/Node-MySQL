64508972bdeeeda89ca7f9e0729f9e57fd86eb65
slr <nilaygupta1998@gmail.com>
2020-10-08 04:12:46 +0530

    removing socketID request

--- socket.js
+++ socket.js
@@ -14,32 +14,25 @@ function socketConnection(io)
             });
         });
         
-        socket.on('messagedetection',(sender,receiver,message)=>{
-            var sql="Select socketID from UserInfo where email=?";
-            mysqlConnection.query(sql,receiver,(err,rows,fields)=>{
+        socket.on('messagedetection',(sender,receiver,message,receiverSocketID)=>{
+
+            console.log("Sender: "+sender+" Receiver: "+receiver+" Message:"+message);
+            const id=receiverSocketID;
+            console.log("Sending to SocketID: "+id);
+
+            const MsgData={
+                sender: sender,
+                receiver: receiver,
+                message: message
+            };
+            
+            socket.to(id).emit('messageToUser', MsgData);
+
+            
+            sql="INSERT INTO msg set ?";
+            mysqlConnection.query(sql,MsgData, (err,rows,fields)=>{
                 if(err) throw err;
-
-                //console.log("Receiver SocketID: "+rows[0].socketID+" : "+socket.id);
-                console.log("Sender: "+sender+" Receiver: "+receiver+" Message:"+message);
-                //const msg = {'sender':sender,'message':message};
-                const id=rows[0].socketID;
-                console.log("Sending to SocketID: "+id);
-
-                const MsgData={
-                    sender: sender,
-                    receiver: receiver,
-                    message: message
-                };
-                
-                socket.to(id).emit('messageToUser', MsgData);
-                //socket.broadcast.emit('messageToUser', MsgData);
-                    
-
-                sql="INSERT INTO msg set ?";
-                mysqlConnection.query(sql,MsgData, (err,rows,fields)=>{
-                    if(err) throw err;
-                    console.log("Message added to databse");
-                });
+                console.log("Message added to databse");
             });
         });
     });
